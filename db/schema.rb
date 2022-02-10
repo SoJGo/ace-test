9ddbacdd1aa13219d2f3401037990475984b02a9
SophieGowen <sophie.j.gowen@gmail.com>
2022-02-10 23:27:58 +0000

    remove user schema

--- db/schema.rb
+++ db/schema.rb
@@ -21,12 +21,4 @@ ActiveRecord::Schema.define(version: 2022_02_07_221819) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "title"
-    t.string "string"
-    t.text "body"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
 end
